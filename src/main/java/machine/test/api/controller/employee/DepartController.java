6eba3f9d1e4477a0d64f5de3c4cd97c1a96f6d3a
selva93 <selvasham93@gmail.com>
2019-10-17 20:33:41 +0530

    change controller name

--- src/main/java/machine/test/api/controller/employee/DepartController.java
+++ src/main/java/machine/test/api/controller/employee/DepartController.java
@@ -1,8 +1,8 @@
 package machine.test.api.controller.employee;
 
 import machine.test.api.controller.GenericController;
-import machine.test.api.model.employee.Department;
-import machine.test.api.service.employee.DepartmentService;
+import machine.test.api.model.employee.DepartModel;
+import machine.test.api.service.employee.DepartService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,12 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/department")
 @Controller
-public class DepartmentController extends GenericController<Department> {
+public class DepartController extends GenericController<DepartModel> {
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    public DepartmentController(DepartmentService departmentService) {
-        super(departmentService);
+    public DepartController(DepartService departService) {
+        super(departService);
     }
 }
