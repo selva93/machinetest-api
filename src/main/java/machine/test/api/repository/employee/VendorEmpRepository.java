f561d2bcc50f5b77dbab6e3015df4ba3770e9949
selva93 <selvasham93@gmail.com>
2019-10-17 20:57:31 +0530

    updated

---
+++ src/main/java/machine/test/api/repository/employee/VendorEmpRepository.java
@@ -0,0 +1,9 @@
+package machine.test.api.repository.employee;
+
+import machine.test.api.model.employee.VendorEmpModel;
+import machine.test.api.repository.GenericRepository;
+
+public interface VendorEmpRepository extends GenericRepository<VendorEmpModel> {
+
+
+}
--- src/main/java/machine/test/api/repository/employee/VendorEmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package machine.test.api.repository.employee;
-
-import machine.test.api.model.employee.VendorEmployee;
-import machine.test.api.repository.GenericRepository;
-
-public interface VendorEmployeeRepository extends GenericRepository<VendorEmployee> {
-
-
-}
