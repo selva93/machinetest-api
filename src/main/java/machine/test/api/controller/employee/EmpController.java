d72835e1e159e0502512c86905620b9021bf9e81
selva93 <selvasham93@gmail.com>
2019-10-17 20:34:50 +0530

    change controller name

--- src/main/java/machine/test/api/controller/employee/EmpController.java
+++ src/main/java/machine/test/api/controller/employee/EmpController.java
@@ -1,8 +1,8 @@
 package machine.test.api.controller.employee;
 
 import machine.test.api.controller.GenericController;
-import machine.test.api.model.employee.Employee;
-import machine.test.api.service.employee.EmployeeService;
+import machine.test.api.model.employee.EmpModel;
+import machine.test.api.service.employee.EmpService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,12 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/employee")
 @Controller
-public class EmployeeController extends GenericController<Employee> {
+public class EmpController extends GenericController<EmpModel> {
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    public EmployeeController(EmployeeService employeeServiceImpl) {
-        super(employeeServiceImpl);
+    public EmpController(EmpService empServiceImpl) {
+        super(empServiceImpl);
     }
 }
