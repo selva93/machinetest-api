1c980c82232e11cc0bc37aa70bf3cc0229162ead
selva93 <selvasham93@gmail.com>
2019-10-17 20:57:05 +0530

    updated

---
+++ src/main/java/machine/test/api/repository/employee/EmpRepository.java
@@ -0,0 +1,9 @@
+package machine.test.api.repository.employee;
+
+import machine.test.api.model.employee.EmpModel;
+import machine.test.api.repository.GenericRepository;
+
+public interface EmpRepository extends GenericRepository<EmpModel> {
+
+
+}
--- src/main/java/machine/test/api/repository/employee/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package machine.test.api.repository.employee;
-
-import machine.test.api.model.employee.Employee;
-import machine.test.api.repository.GenericRepository;
-
-public interface EmployeeRepository extends GenericRepository<Employee> {
-
-
-}
