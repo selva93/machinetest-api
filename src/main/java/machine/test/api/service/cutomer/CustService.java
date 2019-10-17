fd07853b36502c641e098363f7c18fb9e05dcd83
selva93 <selvasham93@gmail.com>
2019-10-18 00:42:55 +0530

    initial commit

--- src/main/java/machine/test/api/service/cutomer/CustService.java
+++ src/main/java/machine/test/api/service/cutomer/CustService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
-public class CustomService {
+public class CustService {
 
     Cache<String, CUSTOMERDETAILS> customerDetailsCache = CacheBuilder.newBuilder().build();
     public CUSTOMERDETAILS saveCustomerDetails(RESPONSE customerInfo) {
