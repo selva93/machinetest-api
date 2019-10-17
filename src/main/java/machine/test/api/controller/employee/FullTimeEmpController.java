a2daa527bd9ed5ca424663f858e31a75c4646125
selva93 <selvasham93@gmail.com>
2019-10-17 20:35:18 +0530

    updated

--- src/main/java/machine/test/api/controller/employee/FullTimeEmpController.java
+++ src/main/java/machine/test/api/controller/employee/FullTimeEmpController.java
@@ -1,8 +1,8 @@
 package machine.test.api.controller.employee;
 
 import machine.test.api.controller.GenericController;
-import machine.test.api.model.employee.FullTimeEmployee;
-import machine.test.api.service.employee.FullTimeEmployeeService;
+import machine.test.api.model.employee.FullTimeEmpModel;
+import machine.test.api.service.employee.FullTimeEmpService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,12 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/fulltimeemployee")
 @Controller
-public class FullTimeEmployeeController extends GenericController<FullTimeEmployee> {
+public class FullTimeEmpController extends GenericController<FullTimeEmpModel> {
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    public FullTimeEmployeeController(FullTimeEmployeeService fullEmployeeService) {
+    public FullTimeEmpController(FullTimeEmpService fullEmployeeService) {
         super(fullEmployeeService);
     }
 }
