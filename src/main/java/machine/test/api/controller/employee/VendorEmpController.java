94aec807eda21abc421d77a37d301b5bf7754287
selva93 <selvasham93@gmail.com>
2019-10-17 20:35:37 +0530

    updated

--- src/main/java/machine/test/api/controller/employee/VendorEmpController.java
+++ src/main/java/machine/test/api/controller/employee/VendorEmpController.java
@@ -1,8 +1,8 @@
 package machine.test.api.controller.employee;
 
 import machine.test.api.controller.GenericController;
-import machine.test.api.model.employee.VendorEmployee;
-import machine.test.api.service.employee.VendorEmployeeService;
+import machine.test.api.model.employee.VendorEmpModel;
+import machine.test.api.service.employee.VendorEmpService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,12 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vendoremployee")
 @Controller
-public class VendorEmployeeController extends GenericController<VendorEmployee> {
+public class VendorEmpController extends GenericController<VendorEmpModel> {
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    public VendorEmployeeController(VendorEmployeeService vendorEmployeeService) {
-        super(vendorEmployeeService);
+    public VendorEmpController(VendorEmpService vendorEmpService) {
+        super(vendorEmpService);
     }
 }
