137ebfc48c1a68d601223cc969ae1f92ab9d8701
selva93 <selvasham93@gmail.com>
2019-10-17 20:56:55 +0530

    updated

---
+++ src/main/java/machine/test/api/repository/employee/DepartRepository.java
@@ -0,0 +1,9 @@
+package machine.test.api.repository.employee;
+
+import machine.test.api.model.employee.DepartModel;
+import machine.test.api.repository.GenericRepository;
+
+public interface DepartRepository extends GenericRepository<DepartModel> {
+
+
+}
--- src/main/java/machine/test/api/repository/employee/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package machine.test.api.repository.employee;
-
-import machine.test.api.model.employee.Department;
-import machine.test.api.repository.GenericRepository;
-
-public interface DepartmentRepository extends GenericRepository<Department> {
-
-
-}
