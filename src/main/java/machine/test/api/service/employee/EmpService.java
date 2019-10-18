9050bb0b55873345d05e0fa7222f159fb4dec90c
selva93 <selvasham93@gmail.com>
2019-10-18 12:26:32 +0530

    initial commit

--- src/main/java/machine/test/api/service/employee/EmpService.java
+++ src/main/java/machine/test/api/service/employee/EmpService.java
@@ -1,15 +1,15 @@
 package machine.test.api.service.employee;
 
-import machine.test.api.model.employee.Department;
-import machine.test.api.repository.employee.DepartmentRepository;
+import machine.test.api.model.employee.EmpModel;
+import machine.test.api.repository.employee.EmpRepository;
 import machine.test.api.service.GenericService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class DepartmentService extends GenericService<Department> {
+public class EmpService extends GenericService<EmpModel> {
     @Autowired
-    public DepartmentService(DepartmentRepository crudRepository) {
+    public EmpService(EmpRepository crudRepository) {
         super(crudRepository);
     }
 }
